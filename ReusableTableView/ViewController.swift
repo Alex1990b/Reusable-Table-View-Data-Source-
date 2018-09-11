e65b9c4b89ccd283dade2bb28c6ac4d2430670d9
Alex1990b <sashabondar999@gmail.com>
2018-09-11 10:51:11 +0300

    Initial Commit

---
+++ ReusableTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReusableTableView
+//
+//  Created by Sasha on 11.09.18.
+//  Copyright © 2018 Sasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
